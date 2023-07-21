bb55e8dc8d73eac5eaf5f12b1657e2c2a809a230
nimishk7 <nimishkhadilkar7@gmail.com>
2023-07-22 00:43:35 +0530

    new commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // TOGGLE NAVBAR ICON 
-let menuIcon=document.querySelector('#menu-icon');
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick=()=>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 
@@ -10,30 +10,35 @@ menuIcon.onclick=()=>{
 
 // SCROLL SECTION
 
-let sections= document.querySelectorAll('section');
-let navLinks= document.querySelectorAll('header nav a')
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
 
-window.onscroll=()=>{
-    // sections.forEach(sec=>{
-    //     let top = window.scrollY;
-    //     let offset= sec.offsetTop - 100;
-    //     let height = sec.offsetHeight;
-    //     let id = sec.getAttribute('id');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset= sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-    //     if (top >= offset && top < offset + height){
-    //         // active navbar links
-    //         navLinks.forEach(links => {
-    //             links.classList.remove('active');
-    //             document.querySelector('header nav a[href*='+ id +']').classList.add('active');
-
-    //         });
-    //     }
-    // });
+        if (top >= offset && top < offset + height){
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+            });
+                // active section Animation
+                sec.classList.add('show-animation');
+        }
+         
+        else{
+            sec.classList.remove('show-animation');
+        }
+    });
 
     let header=document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
-
-// BREAKDOWNS
-
